50f689ca42923c39d5e58916e0fab12ad59952d8
Jodie Gardiner <jodiegardiner@outlook.com>
2016-09-27 12:08:15 +0100

    more scripting

--- js/my_script.js
+++ js/my_script.js
@@ -1,6 +1,6 @@
 // custom js for swapi-project stream 1
 
-// Search for People
+// Search for People name using user input
 $("#searchQuery").on('keyup', function(event) {
 
 	event.preventDefault();
@@ -20,12 +20,39 @@ $("#searchQuery").on('keyup', function(event) {
 		var searchOutput = "";
 
 		$.each(searchItem.results, function(i, sData){
-		console.log(sData);	
-		searchOutput += '<tr><td>' + sData.name + '</td><td>' + sData.gender + '</td><td>' + sData.hair_color + '</td><td>' + sData.eye_color + '</td><td>' + searchItem.results[0].skin_color + '</td></tr>';
+			
+		searchOutput += '<tr data-toggle="modal" data-target="#sw_modal" id="'+sData.url+'"><td>' + sData.name + '</td><td>' + sData.gender + '</td><td>' + sData.hair_color + '</td><td>' + sData.eye_color + '</td><td>' + searchItem.results[0].skin_color + '</td></tr>';
 		})		
 
-		console.log(searchItem);
 
 		$("#search-results").html( searchOutput );
+	});
+})
+
+// Return specific person, format for modal
+$("#search-results").on('click', "tr", function(event){
+
+	var urlID = $(this).attr('id');
+	console.log(urlID);
+
+	$.ajax({
+		url: urlID,
+		dataType: 'json',
+
+	})
+
+	.success(function( personDetail ) {
+		var personOutput = "";
+		console.log(personDetail);
+		
+
+		$("#peopleModalLabel").html(personDetail.name);
+		$("#peopleHeight").html(personDetail.height);
+		$("#peopleWeight").html(personDetail.mass);
+		$("#peopleHair").html(personDetail.hair_color);
+		$("#peopleEyes").html(personDetail.eye_color);
+
+
+
 	});
 })
